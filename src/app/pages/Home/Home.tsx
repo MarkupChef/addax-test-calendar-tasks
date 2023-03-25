5277964f193c5bb8d247eff15d971dfa1f1255f1
markupMashine <rastehaiev.r@gmail.com>
2023-03-25 16:58:58 +0200

    First commit

---
+++ src/app/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad alias asperiores aut corporis, doloribus eius
+        explicabo libero magni nam necessitatibus omnis perferendis reiciendis, sapiente, sint soluta totam veritatis
+        vitae!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
